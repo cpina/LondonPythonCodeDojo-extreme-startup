60a13682d4dd55fb44e71287683795877d91e90c
Carles Pina i Estany <carles@pina.cat>
2013-06-08 00:09:02 +0100

    Add basic example.

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import re
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route("/")
+def answer():
+    q = request.args.get("q", "")
+    print "Question:", q
+
+    m = re.search("what is ([0-9]+) plus ([0-9]+)", q)
+
+    answer = "Unknown"
+
+    if m:
+        answer = str(int(m.group(1)) + int(m.group(2)))
+
+    print "Answer:", answer
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
